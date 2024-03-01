30db05fc7e572a0e9fc769df960e1233a3fae62a
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-03-01 14:38:58 -0500

    Added player footstep sound

---
+++ Assets/Scripts/SFXPlayer.cs
@@ -0,0 +1,26 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+namespace Quinn
+{
+	public class SFXPlayer : MonoBehaviour
+	{
+		[SerializeField]
+		private EventReference Sound;
+
+		[SerializeField]
+		private bool IsAttached;
+
+		public void PlaySound()
+		{
+			if (IsAttached)
+			{
+				AudioManager.Play(Sound, transform);
+			}
+			else
+			{
+				AudioManager.Play(Sound, transform.position);
+			}
+		}
+	}
+}

6c4b289d874108134ef938c82bc7a152b5bd7231
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-03-04 23:19:53 -0500

    Added an extension method for destroying particle systems when they are finished

---
+++ Assets/Scripts/Tools/DestroyOnCondition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Quinn
+{
+	public class DestroyOnCondition : MonoBehaviour
+	{
+		private Func<bool> _condition;
+
+		public IEnumerator Start()
+		{
+			yield return new WaitUntil(() => _condition());
+			Destroy(gameObject);
+		}
+
+		public static void Create(Transform parent, Func<bool> condition)
+		{
+			parent.gameObject.AddComponent<DestroyOnCondition>()._condition = condition;
+		}
+	}
+}
---
+++ Assets/Scripts/Tools/VisualEffectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.VFX;
+
+namespace Quinn
+{
+	public static class VisualEffectExtensions
+	{
+		public static void DestroyOnFinish(this VisualEffect vfx)
+		{
+			DestroyOnCondition.Create(vfx.transform, () => vfx.aliveParticleCount == 0);
+		}
+	}
+}
